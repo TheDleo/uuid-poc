832996b29e82888bcec0ffaf82c74e822ee4d2de
Dan Martin <dmartin@fh.org>
2017-06-23 16:56:19 -0700

    initial commit

---
+++ database/migrations/2017_06_23_000000_create_dans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dans', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dans');
+    }
+}
